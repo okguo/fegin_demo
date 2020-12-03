e06ca591e09e6a4cde0d7896e388095be9635319
Guoyongfu <Guoyongfu.1@hotmail.com>
2020-12-03 09:17:38 +0800

    fegin

--- consumer/consumer-provider/src/main/java/com/okguo/ConsumerProviderApplication.java
+++ consumer/consumer-provider/src/main/java/com/okguo/ConsumerProviderApplication.java
@@ -1,12 +1,13 @@
 package com.okguo;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Hello world!
- *
  */
+@Configuration
 public class ConsumerProviderApplication {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
--- producer/producer-api/src/main/java/com/okguo/ProducerApiApplication.java
+++ producer/producer-api/src/main/java/com/okguo/ProducerApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * Hello world!
- *
  */
 @Configuration
 public class ProducerApiApplication {
